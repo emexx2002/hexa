46f1d952ebdcbfd6ff6c4ce22e8a1daf453d7b8f
Emeka Agu <66725604+emexx2002@users.noreply.github.com>
2021-12-09 08:21:52 +0100

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -3,11 +3,9 @@ import React from "react";
 import classNames from "classnames";
 // react components for routing our app without refresh
 import Link from "next/link";
-// @material-ui/core components
+
 import { makeStyles } from "@material-ui/core/styles";
 import ScrollAnimation from 'react-animate-on-scroll';
-// @material-ui/icons
-// core components
 import Header from "components/Header/Header.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
 import Footer from "components/Footer/Footer.js";
@@ -64,74 +62,7 @@ function home(props) {
         
         </div>
       </Parallax>
-      <div className={classNames(classes.main)} style={{backgroundColor:"#050510", paddingTop:"65px", color:"white"}}>
-          <div className={classes.container}>
-            <GridContainer justify="center">
-              <GridItem xs={12} sm={12} md={6}>
-                <img src="img/block.png" style={{width:"70%", height:"auto"}} />
-              </GridItem>
-              <GridItem xs={12} sm={12} md={6}>
-                
-                <div className={classes.container} style={{paddingTop:"45px"}}>
-                  <div className="animate__animated animate__bounce">
-                  <h1 className={classes.title} style={{ fontSize:"6.5vh"}}>
-                    About Hexa 
-                    </h1>
-                    <p>
-                    Hexa is a new competitor in the GameFi space, built around gaming, with aims to better enhance the synergy between gaming and cryptocurrency.
-                    </p>
-                    <h4>Here is what we offer</h4>
-                    - Online gaming facilities, fitted with play-to-earn functionalities.<br/>
-                    - Telegram group gaming contests.<br/>
-                    - Worldwide Xbox and Play Station gaming contests.<br/>
-                    - An NFT marketplace.<br/>
-                    - Establishment of arcades accepting crypto payments.<br/>
-                    - Launchpad for gaming projects
-
-
-                  </div>
- 
-                    
-  
-                    
-
-                </div>
-              </GridItem>
-              
-            </GridContainer>
-            
-            
-        </div>
-        <div className={classes.container} id="token" style={{paddingTop:"50px"}}>
-          <h2 style={{fontWeight:"bold", textAlign:"center"}}>Tokenomics And Allocation</h2>
-        <GridContainer justify="center" style={{paddingTop:30, paddingBottom:30}}>
-              <GridItem xs={12} sm={12} md={4}>
-                <h3 style={{fontWeight:"bold", textAlign:"center"}}>Token Distribution</h3>
-                <Chart/>
-              </GridItem>
-              <GridItem xs={12} sm={12} md={4}>
-              <h3 style={{fontWeight:"bold", textAlign:"center"}}>presale fund allocation</h3>
-              <Chart2/>
-              </GridItem>
-              <GridItem xs={12} sm={12} md={4}>
-              <h3 style={{fontWeight:"bold", textAlign:"center"}}>Hexanomics</h3>
-              <Chart3/>
-              </GridItem>
-              
-            </GridContainer>
-        </div>
-        <div style={{width:"100%", marginTop:'20px'}}>
-        <div id="roadmap" className={classes.container} style={{paddingTop:"50px"}} >
-          <h2 style={{fontWeight:"bold", textAlign:"center"}}>Roadmap</h2>
-          <h4 style={{textAlign:"center"}}>This timeline details our development goals.</h4>
-        <Timeline />
-        </div>
-
-        </div>
-        
-           
-
-      </div>
+      
       <SectionDownload   />
       <Footer/>
     </div>
