26aca35608fcf6920b6d206ff64112af74e291ce
emexx2002 <aguchukwuemekag@gmail.com>
2021-12-09 08:11:03 +0100

    erroer

--- pages/_error.js
+++ pages/_error.js
@@ -2,11 +2,9 @@ import React, { Component } from "react";
 import Router from "next/router";
 
 export default class _error extends Component {
-  componentDidMount = () => {
-    Router.push("/");
-  };
+  
 
   render() {
-    return <div />;
+    return <div > <h2>Error big one </h2></div>;
   }
 }
